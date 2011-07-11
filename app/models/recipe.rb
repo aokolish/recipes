861f9e9fc343cc963f2d717a519d2dbe17bae5fe
Alex Okolish <alex.okolish@gmail.com>
2011-07-10 22:28:09 -0400

    Setting Chronic Duration up to throw errors (makes it more picky about what you tell it to parse) and using it to create a validation error when the total_time is set to something that cannot be parse. Also, found a bug where a recipe could be saved with 2 pipes separated by whitespace in the directions/ingredients; fixed that.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ActiveRecord::Base
   validates :title, :author, :directions, :ingredients, :presence => true
   validates_uniqueness_of :source_url   # do not import the same recipe twice  
+  validate :ensure_total_time_is_a_time
   before_validation :cleanup_directions_and_ingredients
       
   def self.from_import(url)
@@ -11,14 +12,18 @@ class Recipe < ActiveRecord::Base
   def total_time
     # output total_time in a format that is easy to read e.g. 1800 becomes '30 minutes'
     unless self[:total_time].nil?
-      ChronicDuration.output self[:total_time]
+      begin
+        ChronicDuration.output self[:total_time]
+      rescue
+        self[:total_time] = self.total_time_before_type_cast
+      end
     end
   end
   
   def total_time=(text)
-    if text.class == String
+    begin
       self[:total_time] = ChronicDuration.parse text
-    else
+    rescue
       self[:total_time] = text
     end
   end
@@ -31,9 +36,9 @@ class Recipe < ActiveRecord::Base
     self.ingredients.split('|') 
   end
   
-  def replace_pipes(sep = "\n\n")
+  def replace_pipes(sep = "\n\n", attrs = [:directions,:ingredients])
     # want to do this in some cases. for example, user is about to edit directions
-    [:directions,:ingredients].each do |attr|
+    attrs.each do |attr|
       self[attr].gsub!(/\|/, sep) unless self[attr] =~ /\|/.nil?
     end  
   end
@@ -42,25 +47,31 @@ class Recipe < ActiveRecord::Base
   
   def cleanup_directions_and_ingredients
     [:directions,:ingredients].each do |attr|
-      if self[attr].nil?
-        return nil
-      else
+      unless self[attr].nil?
         # no harm in running this on attributes that are already pipe-delimited
-        attr = self[attr]  
+        str = self[attr]        
+        # change new lines or breaks to pipes
+        str.gsub!(/((\r\n)|\n|(<br>)|(<br \/>)|(<br\/>))+/i, '|')
+        # remove back to back pipes
+        str.squeeze!('|')
+        # strip whitespace
+        str.strip!
+        # remove leading or trailing pipes
+        str.gsub!(/(\A\||\|\z)/, '')
+        # ensure that no blank items are in the string e.g. "...| |...."
+        arr = str.split('|').delete_if {|item| item.blank?}
+        self[attr] = arr.join("|")
       end
-    
-      # change new lines or breaks to pipes
-      attr.gsub!(/((\r\n)|\n|(<br>)|(<br \/>)|(<br\/>))+/i, '|')
-    
-      # remove back to back pipes
-      attr.squeeze('|')
-    
-      # remove leading or trailing pipes
-      attr.gsub!(/(\A\||\|\z)/, '')
-    
-      # strip whitespace
-      final = attr.strip
-      self[attr] = final
+    end
+  end
+  
+  def ensure_total_time_is_a_time
+    unless self[:total_time].nil?
+      begin
+        ChronicDuration.parse total_time
+      rescue
+        errors.add(:total_time, "must be a time such as '1 hour' or '45 minutes'")
+      end
     end
   end
   
--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module Recipes
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    ChronicDuration.raise_exceptions = true
   end
 end
