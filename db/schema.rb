6d446117d1ae1b7bf09dced7a26abfc9528126cb
Alex Okolish <alex.okolish@gmail.com>
2011-09-23 07:37:16 -0400

    schema updated with users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110710171558) do
+ActiveRecord::Schema.define(:version => 20110922114433) do
 
   create_table "recipes", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(:version => 20110710171558) do
     t.string   "yield"
     t.text     "ingredients"
     t.text     "directions"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
