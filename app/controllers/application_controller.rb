325523aa2babcb318e66c9806a86fc7dcfa12f6c
Alex Okolish <alex.okolish@gmail.com>
2011-09-23 07:38:05 -0400

    session logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery  
   before_filter :prepare_for_mobile
+  before_filter :authorize
+  helper_method :current_user
 
   private
 
@@ -16,5 +18,15 @@ class ApplicationController < ActionController::Base
   def prepare_for_mobile
     session[:mobile_param] = params[:mobile] if params[:mobile]
     request.format = :mobile if mobile_device?
+  end
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def authorize
+    if current_user.nil?
+      redirect_to login_url, :notice => "Please log in"
+    end
   end
 end
