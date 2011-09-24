fb2f15e11495842a0c11a3fcc7fdd8c2d888be50
Alex Okolish <alex.okolish@gmail.com>
2011-09-24 19:21:43 -0400

    when a user is created, add their user id to the session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, :notice => "Signed Up!"
     else
       render "new"
