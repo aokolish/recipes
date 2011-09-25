472d1a366fced36df34f24373b4f5dba9eb922e1
Alex Okolish <alex.okolish@gmail.com>
2011-09-24 22:45:59 -0400

    refactored validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,10 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   attr_accessor :password
   before_save :encrypt_password
-  
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
-  validates :password, :length => { :minimum => 4 }
-  validates_presence_of :email
-  validates_uniqueness_of :email
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create, :message => 'is an invalid format'
+
+  validates :password, :presence => true, :confirmation => true, :length => { :minimum => 4 }
+  validates :email, :presence => true, :uniqueness => true, 
+            :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => 'is an invalid format' }
   
   def self.authenticate(email, password)
     user = find_by_email(email)
