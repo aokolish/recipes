67b854ae90fae2f83a7051df156d2cf36b008cce
Alex Okolish <alex.okolish@gmail.com>
2011-09-24 22:45:24 -0400

    removing comments from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -14,18 +14,4 @@ Recipes::Application.routes.draw do
   resources :sessions
   
   root :to => "recipes#index"
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
 end
