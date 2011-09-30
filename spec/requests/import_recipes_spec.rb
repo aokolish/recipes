209797946b51bf4cdefd997c8c9d83967bbfa0af
Alex Okolish <alex.okolish@gmail.com>
2011-09-30 07:58:20 -0400

    fixing this spec

--- spec/requests/import_recipes_spec.rb
+++ spec/requests/import_recipes_spec.rb
@@ -1,6 +1,21 @@
 require 'spec_helper'
 
 describe "ImportRecipes" do
+  let(:user) { Factory(:user) }
+  
+  # there is probably a better way to get a user logged in
+  before(:all) do
+    user.password = 'aoeu'
+    user.save
+  end
+  
+  before(:each) do 
+    visit login_path
+    fill_in "email", :with => user.email
+    fill_in "password", :with => user.password
+    click_button "Log in"
+  end
+  
   describe "desktop version" do
     it "imports a recipe via scraping" do      
       visit import_recipes_path
@@ -13,25 +28,25 @@ describe "ImportRecipes" do
     end
     
     it "tells you when the recipe has already been imported" do
-      visit import_recipes_path
-      fill_in "source_url", :with => "http://www.foodnetwork.com/example"
-      click_button "Submit"
-      page.should have_content("Recipe was successfully created")
-      
-      visit import_recipes_path
-      fill_in "source_url", :with => "http://www.foodnetwork.com/example"
-      click_button "Submit"
-      page.should have_content("Sorry, that recipe has already been imported")
-    end
-  
-    it "cannot scrape all sites" do
-      visit import_recipes_path
-      fill_in "source_url", :with => "google.com"
-      click_button "Submit"
-      page.should have_content("Sorry, there was a problem creating a recipe from")
-    end
+       visit import_recipes_path
+       fill_in "source_url", :with => "http://www.foodnetwork.com/example"
+       click_button "Submit"
+       page.should have_content("Recipe was successfully created")
+       
+       visit import_recipes_path
+       fill_in "source_url", :with => "http://www.foodnetwork.com/example"
+       click_button "Submit"
+       page.should have_content("Sorry, that recipe has already been imported")
+     end
+       
+     it "cannot scrape all sites" do
+       visit import_recipes_path
+       fill_in "source_url", :with => "google.com"
+       click_button "Submit"
+       page.should have_content("Sorry, there was a problem creating a recipe from")
+     end
   end
-  
+ 
   describe "mobile version" do
     it "imports a recipe via scraping" do
       visit import_recipes_path(:mobile => '1')
