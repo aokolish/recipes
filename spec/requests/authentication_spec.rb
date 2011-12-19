c6461895d6aa429f0f5d518021872b9ec2f638fe
Alex Okolish <alex.okolish@gmail.com>
2011-12-19 08:00:21 -0500

    updated test to reflect current authentication policy

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -4,10 +4,10 @@ describe "authentication" do
 
   it "requires you to log in for these routes" do
     # put routes that require authentication into this array
-    routes = [new_recipe_path, import_recipes_path]
+    routes = [new_recipe_path]
     require_authentication(routes)
-  end 
-  
+  end
+
   def require_authentication(routes)
     routes.each do |route|
       visit route
