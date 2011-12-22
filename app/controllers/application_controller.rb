7ecf85a145010c035899a3b859381410e35adece
Alex Okolish <alex.okolish@gmail.com>
2011-12-22 07:23:49 -0500

    remove mobile logic from app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery  
-  before_filter :prepare_for_mobile
+  protect_from_forgery
   before_filter :authorize
   helper_method :current_user
 
   private
 
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile|webOS/
-    end
-  end
-  helper_method :mobile_device?
-
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    #turning off mobile for the time being
-    #I'm going to rethink my mobile strategy
-    #request.format = :mobile if mobile_device?
-  end
-  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  
+
   def authorize
     if current_user.nil?
       redirect_to login_url, :notice => "Please log in or <a href='#{url_for(new_user_path)}'>create an account</a>.".html_safe
