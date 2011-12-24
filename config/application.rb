0eaa8eaed13f8b681ce2c6a04dd291ab56981fb6
Alex Okolish <alex.okolish@gmail.com>
2011-12-24 16:43:43 -0500

    changes to get fonts working with the asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Recipes
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.precompile += ['mobile.js', 'mobile.css']
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
