828c45f07aa392506851c5553389ceac260f5572
Alex Okolish <alex.okolish@gmail.com>
2012-01-10 21:26:42 -0500

    new homepage rule

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Recipes::Application.routes.draw do
+  get "static/home"
+
   get "login" => "sessions#new"
   get "logout" => "sessions#destroy"
 
@@ -9,9 +11,9 @@ Recipes::Application.routes.draw do
       post 'create_from_import', :as => :create_from_import
     end
   end
-  
+
   resources :users
   resources :sessions
-  
-  root :to => "recipes#index"
+
+  root :to => "static#home"
 end
