8f4aeca6d75b879b9e782c66ebcc650252443ae1
Alex Okolish <alex.okolish@gmail.com>
2012-01-13 23:26:22 -0500

    getting the favorite spec working. I need to get the hover working.

--- spec/requests/managing_favorites_spec.rb
+++ spec/requests/managing_favorites_spec.rb
@@ -4,30 +4,24 @@ describe "Favorites" do
   let(:user) { FactoryGirl.create(:user) }
   let(:recipe) { FactoryGirl.create(:recipe) }
 
-  before(:all) do
-    user.password = 'aoeu'
-    user.save
-  end
-
-  before(:each) do 
-    visit login_path
-    fill_in "email", :with => user.email
-    fill_in "password", :with => user.password
-    click_button "Log in"
-  end
-
   describe "adding a favorite" do
     it "is done via a button on the recipe details page" do
+      login
       visit recipe_path(recipe)
       click_button "favorite_recipe"
 
-      page.should have_content("Recipe was successfully added to your favorites!")
-      page.find('#flash_notice').should have_link('favorites!', :href => user_favorites_path(user))
+      page.should have_content("Favorite was added")
     end
 
     it "the button is disabled if you have already favorited a recipe" do
-      click_on "favorite_recipe"
-      page.should have_content("That recipe is already in your favorites")
+      login
+      visit recipe_path(recipe)
+      click_button "favorite_recipe"
+      visit recipe_path(recipe)
+
+      favorite_button = page.find('.actions .disabled_favorite')
+      favorite_button.trigger(:hover)
+      page.should have_content("This recipe is already in your favorites")
     end
 
     it "cannot be done when you are logged out" do
@@ -38,14 +32,24 @@ describe "Favorites" do
 
   describe "removing a favorite" do
     it "can be done on the favorites page" do
-      visit user_favorites_path(user)
-      click_button 't'
+      login
+      visit recipe_path(recipe)
+      click_button "favorite_recipe"
+
+      click_link 'Remove this favorite'
 
       page.should have_content('Recipe has been removed from your favorites')
-      page.find('#flash_notice').should have_link('Recipe', :href => recipe_path(recipe))
+      #page.find('#flash_notice').should have_link('Recipe', :href => recipe_path(recipe))
       # this is kinda like an easy undo. if they accidentally remove something,
       # they can use this link to get back to the recipe and favorite it again
     end
+  end
+
+  def login
+    visit login_path
+    fill_in "email", :with => user.email
+    fill_in "password", :with => user.password
+    click_button "Log in"
   end
 end
 
