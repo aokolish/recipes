89a3c5ba4d60aff3b55f2aa6967d8fa1b8b51ae4
Alex Okolish <alex.okolish@gmail.com>
2012-01-28 12:19:21 -0500

    commenting out specs that no longer mean anything. perhaps, I should just remove them; the whole spec needs rethought

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,62 +11,62 @@ describe Recipe do
       @from_db = Recipe.find_by_title "Scrambled Eggs"
     end
 
-    it "finds a recipe by title", :broken => true do
-      result = Recipe.search "Scrambled Eggs"
-      result.should include(@from_db)
-    end
+    #it "finds a recipe by title" do
+    #  result = Recipe.search "Scrambled Eggs"
+    #  result.should include(@from_db)
+    #end
 
-    it "finds a recipe by author", :broken => true do
-      result = Recipe.search "Alex"
-      result.should include(@from_db)
-    end
+    #it "finds a recipe by author" do
+    #  result = Recipe.search "Alex"
+    #  result.should include(@from_db)
+    #end
 
-    it "finds a recipe by directions", :broken => true do
-      result = Recipe.search "cook eggs in a"
-      result.should include(@from_db)
-    end
+    #it "finds a recipe by directions" do
+    #  result = Recipe.search "cook eggs in a"
+    #  result.should include(@from_db)
+    #end
 
-    it "finds a recipe by ignredients", :broken => true do
-      result = Recipe.search "sausage basil"
-      result.should include(@from_db)
-    end
+    #it "finds a recipe by ignredients" do
+    #  result = Recipe.search "sausage basil"
+    #  result.should include(@from_db)
+    #end
 
-    it "returns an empty array for a bogus search", :broken => true do
-      result = Recipe.search "adsfasdfasdf"
-      result.should be_empty
-    end
+    #it "returns an empty array for a bogus search" do
+    #  result = Recipe.search "adsfasdfasdf"
+    #  result.should be_empty
+    #end
 
-    it "handles exception raised by invalid queries", :broken => true do
-      # some queries are not allowed such as '*' by the external api
-      result = Recipe.search "*"
-      # result.should be_empty
-    end
+    #it "handles exception raised by invalid queries" do
+    #  # some queries are not allowed such as '*' by the external api
+    #  result = Recipe.search "*"
+    #  # result.should be_empty
+    #end
 
-    it "should update search index after destroying a recipe", :broken => true do
-      create_another_recipe
-      result = Recipe.search "Anonymous"
-      result.should include(@other_recipe) 
+    #it "should update search index after destroying a recipe" do
+    #  create_another_recipe
+    #  result = Recipe.search "Anonymous"
+    #  result.should include(@other_recipe) 
 
-      recipe = Recipe.last
-      recipe.destroy
+    #  recipe = Recipe.last
+    #  recipe.destroy
 
-      result = Recipe.search "Anonymous"
-      result.should_not include(@other_recipe)
-    end
+    #  result = Recipe.search "Anonymous"
+    #  result.should_not include(@other_recipe)
+    #end
 
-    it "should update search index after saving a recipe", :broken => true do
-      create_another_recipe
-      result = Recipe.search "Anonymous"
-      result.should include(@other_recipe)
+    #it "should update search index after saving a recipe" do
+    #  create_another_recipe
+    #  result = Recipe.search "Anonymous"
+    #  result.should include(@other_recipe)
 
-      # update and search based on updated field
-      recipe = Recipe.last
-      recipe.author = 'Captain America'
-      recipe.save
+    #  # update and search based on updated field
+    #  recipe = Recipe.last
+    #  recipe.author = 'Captain America'
+    #  recipe.save
 
-      result = Recipe.search "Captain America"
-      result.should include(@other_recipe)
-    end
+    #  result = Recipe.search "Captain America"
+    #  result.should include(@other_recipe)
+    #end
 
     def create_another_recipe
       @other_recipe = Recipe.create( :title => "Grilled Cheese", :author => "Anonymous", 
