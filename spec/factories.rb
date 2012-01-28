435b7e90e92caf26edcdc191c81809c4aca74e64
Alex Okolish <alex.okolish@gmail.com>
2012-01-28 12:18:42 -0500

    updating factories and trying to figure out how to deal with associations

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,5 +8,14 @@ Factory.define :recipe do |f|
   f.author "Swedish Chef"
   f.directions "cut things up|heat things|stir things|enjoy"
   f.ingredients "2 carrots|1 onion|1 pound stew meat"
+  f.association :user
 end
 
+Factory.define :author, :parent => :user do |f|
+  f.association :authored_recipes, :factory => :recipe
+end
+
+Factory.define :favorite do |f|
+  f.association :recipe
+  f.association :user
+end
