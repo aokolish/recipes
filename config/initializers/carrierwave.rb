afa6922085a930f898aa0ccf7847895ea649b688
Alex Okolish <alex.okolish@gmail.com>
2012-02-01 22:15:45 -0500

    carrier wave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,3 +11,16 @@ module CarrierWave
 
   end
 end
+
+CarrierWave.configure do |config|
+  # keys are stored as heroku config vars
+  # see https://aws-portal.amazon.com/gp/aws/securityCredentials for credentials
+  # http://devcenter.heroku.com/articles/config-vars
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['S3_KEY'],
+    :aws_secret_access_key  => ENV['S3_SECRET'],
+    :region                 => 'us-east-1'
+  }
+  config.fog_directory  = 'recipes1.heroku'
+end
