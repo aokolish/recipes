4afd27cac42e0d91e3fdf1a40832b79e68fb8421
Alex Okolish <alex.okolish@gmail.com>
2012-02-02 22:06:38 -0500

    simple recipe integration spec - probably should expand upon this

---
+++ spec/requests/recipes_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Recipes" do
+  let(:recipe) { Factory.build(:recipe) }
+
+  describe "GET /recipes" do
+    it "displays tasks" do
+      Factory.create(:recipe, :title => "beer cheese soup")
+      visit recipes_path
+      page.should have_content("beer cheese soup")
+    end
+
+    it "supports js", :js => true do
+      visit recipes_path
+    end
+  end
+
+  describe "POST /recipe" do
+    it "creates task" do
+      login
+
+      visit new_recipe_path
+      fill_in "Title", :with => "chicken soup"
+      fill_in "Ingredients", :with => "1 chicken\n2 carrots\n1 onion"
+      fill_in "Directions", :with => "do some stuff..."
+      fill_in "Author", :with => "budding chef"
+
+      click_button "Submit"
+      page.should have_content("Recipe was successfully created.")
+      page.should have_content("chicken soup")
+    end
+  end
+end
+
