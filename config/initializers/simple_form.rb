cda9a211a23b90526b801d1e14ffbb5272c5a953
Alex Okolish <alex.okolish@gmail.com>
2012-03-04 16:53:11 -0800

    update config syntax for newer version of simple_form

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -2,19 +2,15 @@
 SimpleForm.setup do |config|
   # Components used by the form builder to generate a complete input. You can remove
   # any of them, change the order, or even add your own components to the stack.
-  config.components = [ :placeholder, :label_input, :hint ]
+  # config.components = [ :placeholder, :label_input, :hint ]
 
-  # Default tag used on hints.
-  # config.hint_tag = :span
-
-  # CSS class to add to all hint tags.
-  # config.hint_class = :hint
-
-  # CSS class used on errors.
-  # config.error_class = :error
-
-  # Default tag used on errors.
-  # config.error_tag = :span
+  config.wrappers :tag => :div, :class => :field,
+                   :error_class => :field_with_errors do |b|
+    b.use :placeholder
+    b.use :hint,  :wrap_with => { :tag => :span, :class => :hint }
+    b.use :label_input
+    b.use :error, :wrap_with => { :tag => :span, :class => :error }
+  end
 
   # Method used to tidy up errors.
   # config.error_method = :first
@@ -27,22 +23,6 @@ SimpleForm.setup do |config|
 
   # ID to add for error notification helper.
   # config.error_notification_id = nil
-
-  # You can wrap all inputs in a pre-defined tag.
-  # config.wrapper_tag = :div
-
-  # CSS class to add to all wrapper tags.
-  config.wrapper_class = :field
-
-  # CSS class to add to the wrapper if the field has errors.
-  # config.wrapper_error_class = :field_with_errors
-
-  # You can wrap a collection of radio/check boxes in a pre-defined tag, defaulting to none.
-  # config.collection_wrapper_tag = nil
-
-  # You can wrap each item in a collection of radio/check boxes with a tag, defaulting to span.
-  # config.item_wrapper_tag = :span
-
   # Series of attempts to detect a default label method for collection.
   # config.collection_label_methods = [ :to_label, :name, :title, :to_s ]
 
