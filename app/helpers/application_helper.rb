0f2078e39c9cbb34932cf37533aae3bce33fbf6d
Alex Okolish <alex.okolish@gmail.com>
2012-06-23 22:40:36 -0700

    sortable search results wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def sortable(column, title)
+    if column == 'best_match'
+      css_class = "current" if params[:sort] == nil
+      link_to title, {:search => params[:search]}, {:class => css_class}
+    else
+      css_class = "current #{sort_direction}" if column == sort_column
+      direction = sort_direction == "asc" ? "desc" : "asc"
+      search_params = {:sort => column, :direction => direction, :search => params[:search]}
+      link_to title, search_params, {:class => css_class}
+    end
+  end
 end
