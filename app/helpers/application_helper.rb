c4b1d1638a742ccefac2a553070d73ed0e8b0875
Alex Okolish <alex.okolish@gmail.com>
2012-07-10 20:22:47 -0700

    styles and changes for search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
   def sortable(column, title=nil)
     title ||= column.titleize
+    css_class = 'btn btn-large '
     if column == 'best_match' # not a real column
-      css_class = "current" if params[:sort] == nil
+      css_class << "active" if params[:sort] == nil
       link_to title, {:search => params[:search]}, {:class => css_class}
     else
       current_direction = sort_direction
-      css_class = "current #{current_direction}" if column == sort_column
+      css_class << "active #{current_direction}" if column == sort_column
       new_direction = current_direction == "asc" ? "desc" : "asc"
       search_params = {:sort => column, :direction => new_direction, :search => params[:search]}
       link_to title, search_params, {:class => css_class}
