55f426a51ae51be394ce729b5f07b55178a6cd13
Alex Okolish <alex.okolish@gmail.com>
2012-07-29 17:56:52 -0700

    remove test for page that no longer exists

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -1,12 +1,4 @@
 require 'spec_helper'
 
 describe StaticController do
-
-  describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
-  end
-
 end
