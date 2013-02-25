f030d44530517910ae581d8e24c17694740606b9
Alex Okolish <alex.okolish@gmail.com>
2013-02-24 21:16:49 -0800

    change how rating stars are outputted

--- app/helpers/review_helper.rb
+++ app/helpers/review_helper.rb
@@ -1,14 +1,14 @@
 module ReviewHelper
   def rating_stars(rating)
-    (1..5).inject('') do |memo, i|
+    return '' unless rating > 0
+
+    (1..rating.ceil).inject('') do |memo, i|
       memo += content_tag(:span) do
         css = ''
         if rating > 0
           if rating < 1
             css = '-half'
           end
-        else
-          css = '-empty'
         end
         rating -= 1
         content_tag(:i, nil, class: "icon-star#{css}")
--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -6,9 +6,13 @@ describe ReviewHelper do
       helper.rating_stars(5.0).should eq(
         "<span><i class=\"icon-star\"></i></span><span><i class=\"icon-star\"></i></span><span><i class=\"icon-star\"></i></span><span><i class=\"icon-star\"></i></span><span><i class=\"icon-star\"></i></span>")
       helper.rating_stars(3.0).should eq(
-        "<span><i class=\"icon-star\"></i></span><span><i class=\"icon-star\"></i></span><span><i class=\"icon-star\"></i></span><span><i class=\"icon-star-empty\"></i></span><span><i class=\"icon-star-empty\"></i></span>")
+        "<span><i class=\"icon-star\"></i></span><span><i class=\"icon-star\"></i></span><span><i class=\"icon-star\"></i></span>")
       helper.rating_stars(2.5).should eq(
-        "<span><i class=\"icon-star\"></i></span><span><i class=\"icon-star\"></i></span><span><i class=\"icon-star-half\"></i></span><span><i class=\"icon-star-empty\"></i></span><span><i class=\"icon-star-empty\"></i></span>")
+        "<span><i class=\"icon-star\"></i></span><span><i class=\"icon-star\"></i></span><span><i class=\"icon-star-half\"></i></span>")
+    end
+
+    it "returns an empty string if the rating is less than zero" do
+      helper.rating_stars(0).should eq("")
     end
   end
 end
