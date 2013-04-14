8b5f8559944ea5b554e98ec565b682257e0184f1
Alex Okolish <alex.okolish@gmail.com>
2013-04-13 20:53:59 -0700

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130401024936) do
+ActiveRecord::Schema.define(:version => 20130414035003) do
 
   create_table "favorites", :force => true do |t|
     t.integer  "recipe_id"
